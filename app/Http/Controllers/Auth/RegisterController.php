9deac03123d0b06d7fbf22d230482425ed148357
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-24 01:28:24 +0300

    Refactored Migration & Controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,6 +68,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'username' => $data['username'],
             'password' => Hash::make($data['password']),
         ]);
     }
