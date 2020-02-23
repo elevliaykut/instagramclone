ad54d8fc20f1b22484195c28c80c0ccb5b29091c
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-24 00:25:54 +0300

    Personal Section Build

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,15 @@
         <div class="col-3 p-5">
             <img src="https://instagram.fbtz1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83037556_621095101795025_4514207238813908992_n.jpg?_nc_ht=instagram.fbtz1-1.fna.fbcdn.net&_nc_ohc=i0VND3GNJJ8AX_zEGxF&oh=cd8f424153593a7615c06d6d3f9e3140&oe=5E887011" class="rounded-circle">
         </div>
-        <div class="col-9">
-
+        <div class="col-9 pt-5">
+            <div>
+                <h1>Aykut Elevli</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>100</strong> posts</div>
+                <div class="pr-5"><strong>296</strong> followers</div>
+                <div class="pr-5"><strong>278</strong> following</div>
+            </div>
         </div>
     </div>
 </div>
