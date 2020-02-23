bfa392a9117e5d677327a78a504d2d5ce88eca62
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-24 00:39:05 +0300

    Personal Section Build

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,19 @@
         </div>
         <div class="col-9 pt-5">
             <div>
-                <h1>Aykut Elevli</h1>
+                <h1>elevliaykut</h1>
             </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>100</strong> posts</div>
                 <div class="pr-5"><strong>296</strong> followers</div>
                 <div class="pr-5"><strong>278</strong> following</div>
+            </div>
+            <div>
+                <div class="pt-2 pb-2"><strong>Aykut Elevli</strong></div>
+                <div>- Lublin University Of Technology</div>
+                <div>- Computer Engineering</div>
+                <div>- Citizen of the world 🇹🇷🇳🇱🇫🇷🇧🇪🇩🇪🇭🇺🇸🇰🇵🇹🇪🇸🇦🇹🇱🇹🇳🇴🇺🇦🇨🇿</div>
+                <div><a href="https://aykutelevli.me/">aykutelevli.me</a></div>
             </div>
         </div>
     </div>
