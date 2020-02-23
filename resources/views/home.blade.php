b10ffb3097cfe7aeeb6ef794fbb1e19abcf6f60c
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-24 00:15:39 +0300

    image section build

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fbtz1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83037556_621095101795025_4514207238813908992_n.jpg?_nc_ht=instagram.fbtz1-1.fna.fbcdn.net&_nc_ohc=i0VND3GNJJ8AX_zEGxF&oh=cd8f424153593a7615c06d6d3f9e3140&oe=5E887011" class="rounded-circle">
+        </div>
+        <div class="col-9">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
         </div>
     </div>
 </div>
