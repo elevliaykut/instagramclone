43cdf19dd9f2fba92983c59e16cd027484b221af
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-01 23:28:59 +0300

    posts moedl added

--- app/User.php
+++ app/User.php
@@ -37,7 +37,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function profile(){
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function profile()
+    {
         return $this->hasOne(Profile::class);
     }
 }
