226689e6b2693c49d420d876c96a12e47a290e9d
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-02 00:31:16 +0300

    Added btn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,9 @@
             <img src="https://instagram.fbtz1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83037556_621095101795025_4514207238813908992_n.jpg?_nc_ht=instagram.fbtz1-1.fna.fbcdn.net&_nc_ohc=i0VND3GNJJ8AX_zEGxF&oh=cd8f424153593a7615c06d6d3f9e3140&oe=5E887011" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div>
+            <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ auth()->user()->username }}</h1>
+                <a href="#">Add New Post</a>
             </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>100</strong> posts</div>
