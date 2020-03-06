6222f58b55a477d8781cf1c1d4983ea81a6bcccf
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-06 17:21:10 +0300

    PostForm Refactored

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,18 +2,25 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-8 offset-2">
-            <div class="form-group row">
-                <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
-                <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" name="caption" value="{{ old('caption') }}" autocomplete="caption" autofocus>
-                @error('caption')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
+    <form action="">
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="form-group row">
+                    <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+                    <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" name="caption" value="{{ old('caption') }}" autocomplete="caption" autofocus>
+                    @error('caption')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label">Post Image</label>
+                    <input type="file" class="form-control-file" id="image" name="image">
+                </div>
             </div>
         </div>
-    </div>
+
+    </form>
 </div>
 @endsection
