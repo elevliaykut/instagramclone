0d8dfeb1567bec78e1fdc60c25e890609215b72b
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-06 16:58:21 +0300

    Image Caption created

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,20 @@
 
 @section('content')
 <div class="container">
-    
+    <div class="row">
+        <div class="col-8 offset-2">
+            <div class="form-group row">
+                <label for="caption" class="col-md-4 col-form-label text-md-right">Image Caption</label>
+                <div class="col-md-6">
+                    <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" name="caption" value="{{ old('caption') }}" autocomplete="caption" autofocus>
+                    @error('caption')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
