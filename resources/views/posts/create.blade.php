23a68ee34a3e977d21c3a36049799daad86ec319
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-07 12:47:09 +0300

    Refactored PostForm

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,9 +2,12 @@
 
 @section('content')
 <div class="container">
-    <form action="">
+    <form action="/p" enctype="multipart/form-data" method="post">
         <div class="row">
             <div class="col-8 offset-2">
+                <div class="row">
+                    <h1>Add New Post</h1>
+                </div>
                 <div class="form-group row">
                     <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
                     <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" name="caption" value="{{ old('caption') }}" autocomplete="caption" autofocus>
@@ -17,10 +20,17 @@
                 <div class="row">
                     <label for="image" class="col-md-4 col-form-label">Post Image</label>
                     <input type="file" class="form-control-file" id="image" name="image">
+                    @error('image')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message->first('image') }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="row pt-4">
+                    <button class="btn btn-primary">Add New Post</button>
                 </div>
             </div>
         </div>
-
     </form>
 </div>
 @endsection
