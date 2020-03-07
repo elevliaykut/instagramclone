c212bff5e09f58c3c3f67287fc1409cf3e93acb3
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-07 15:11:39 +0300

    Refactored PostController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
     {
         return view('posts.create');
