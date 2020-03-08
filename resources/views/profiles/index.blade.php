02cd9d61b33a90fe9e3d4587f2dd0a4fe3030d99
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-08 22:17:21 +0300

    Authorization improvement

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,9 +9,15 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ auth()->user()->username }}</h1>
+
+                @can ('update', $user->profile)
                 <a href="/p/create">Add New Post</a>
+                @endcan
+                
             </div>
+            @can('update', $user->profile)
             <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            @endcan
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>296</strong> followers</div>
