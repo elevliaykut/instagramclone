2ad9b3c3732a6eed9e2a77eb8d8bfdec09897fed
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-09 00:03:17 +0300

    Refactored profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://instagram.fbtz1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83037556_621095101795025_4514207238813908992_n.jpg?_nc_ht=instagram.fbtz1-1.fna.fbcdn.net&_nc_ohc=i0VND3GNJJ8AX_zEGxF&oh=cd8f424153593a7615c06d6d3f9e3140&oe=5E887011" class="rounded-circle">
+            <img src="/storage/{{ $user->profile->image }}" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
@@ -13,7 +13,7 @@
                 @can ('update', $user->profile)
                 <a href="/p/create">Add New Post</a>
                 @endcan
-                
+
             </div>
             @can('update', $user->profile)
             <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
