143c269422b61620019e4f904eb06cd3ebaf1859
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-14 17:43:02 +0300

    image Array refactored

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -52,13 +52,15 @@ class ProfilesController extends Controller
             $imagePath = request('image')->store('profile','public');
             $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000,1000);
             $image->save();
+
+            $imageArray = ['image' => $imagePath];
         }
 
         // dd($data);
 
         auth()->user()->profile->update(array_merge(
             $data,
-            ['image' => $imagePath]
+            $imageArray ?? []
         ));
 
         return redirect("/profile/{$user->id}");
