64ef8d981da24446b72599759bcaf8bf2891503a
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-14 13:24:33 +0300

    dynamic profil image active

--- app/Profile.php
+++ app/Profile.php
@@ -8,6 +8,12 @@ class Profile extends Model
 {
     protected $guarded = [];
 
+    public function profileimage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/ZYiDQVWHmd3KXcBQlubQ0cxy3lgwiBUsnUSmRm9o.png';
+        return '/storage/' . $imagePath;
+    }
+
     public function user(){
         return $this->belongsTo(User::class);
     }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,7 +10,7 @@
             <div>
                 <div class="d-flex align-items-center">
                     <div class="pr-3">
-                        <img src="/storage/{{ $post->user->profile->image }}" class="rounded-circle w-100" style="max-width: 50px">
+                        <img src="{{ $post->user->profile->profileimage() }}" class="rounded-circle w-100" style="max-width: 50px">
                     </div>
                     <div>
                         <div class="font-weight-bold">
