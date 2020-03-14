6c71d13b0adbe2556b5dd8e272b7a3ffd8b2ed2c
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-14 23:11:14 +0300

    Profiles Controller refactored

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -10,7 +10,7 @@
             <div class="d-flex justify-content-between align-items-baseline">
                 <div class="d-flex align-items-center pb-4">
                     <div class="h4">{{ auth()->user()->username }}</div>
-                    <follow-button user-id="{{ $user->id }}"></follow-button>
+                    <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
                 </div>
                 @can ('update', $user->profile)
                 <a href="/p/create">Add New Post</a>
