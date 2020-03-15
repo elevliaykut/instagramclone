ee27dffca6a7a0faff662a2faf6444c02e784eb7
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-15 12:30:00 +0300

    improvements posts lists page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,14 +4,14 @@
 <div class="container">
     @foreach($posts as $post)
     <div class="row">
-        <div class="col-8 offset-2">
+        <div class="col-6 offset-3">
             <a href="/profile/{{ $post->user->id }}">
                 <img src="/storage/{{ $post->image }}" class="w-100">
             </a>
         </div>
     </div>
     <div class="row pt-2 pb-4">
-        <div class="col-8 offset-2">
+        <div class="col-6 offset-3">
             <div>
                 <p>
                     <span class="font-weight-bold">
