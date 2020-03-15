28a879d432184544680661d6a999f0bfbf9eaf80
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-15 13:05:28 +0300

    Cached refactored

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -24,10 +24,10 @@ class ProfilesController extends Controller
         );
 
         $followersCount = Cache::remember(
-            'count.followers.' . $user->id,
+            'count.following.' . $user->id,
             now()->addSeconds(30),
             function () use ($user) {
-                return $user->profile->followers->count();
+                return $user->profile->following->count();
             }
         );
 
